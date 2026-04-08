418d5d7619865c7cfcc6f624e8ff1c10b36011fc
Shantanu Jumale <shantanujumale92@gmail.com>
2026-04-08 10:18:09 +0530

    Add files via upload

---
+++ Practical_14/fact.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Enter a number:"
+read n
+
+fact=1
+i=1
+
+while [ $i -le $n ]
+do
+  fact=$((fact * i))
+  i=$((i + 1))
+done
+
+echo "Factorial of $n is $fact"
---
+++ Practical_14/table.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Enter a number:"
+read n
+
+i=1
+while [ $i -le 10 ]
+do
+  echo "$n x $i = $((n * i))"
+  i=$((i + 1))
+done
